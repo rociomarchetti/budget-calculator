d91985366412c0be9979c8c02034cb273ae5056d
Rocío Marchetti <marchettirociob@gmail.com>
2023-03-20 14:58:35 +0100

    Update visuals

--- src/app/budget/budget-list/budget-list.component.ts
+++ src/app/budget/budget-list/budget-list.component.ts
@@ -5,6 +5,7 @@ import { BudgetService } from './../../services/budget.service';
 @Component({
   selector: 'app-budget-list',
   templateUrl: 'budget-list.component.html',
+  styleUrls: ['budget-list.component.css']
 })
 export class BudgetListComponent implements OnInit {
   constructor(public BudgetService: BudgetService) {}
